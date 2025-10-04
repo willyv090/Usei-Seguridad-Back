3f9662736a8711077356d716b2ff33f8d9caa4a6
Rosario Calisaya <misa26amane@gmail.com>
2025-10-03 20:14:07 -0400

    arreglando archivos

--- Backend/src/main/java/com/usei/usei/UseiApplication.java
+++ Backend/src/main/java/com/usei/usei/UseiApplication.java
@@ -2,17 +2,9 @@ package com.usei.usei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UseiApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(UseiApplication.class);
-	}
-
+public class UseiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UseiApplication.class, args);
 	}
